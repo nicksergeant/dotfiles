190a4eada2de9d29428c375303f4352d6d7862e3
Nick Sergeant <nick@nicksergeant.com>
2013-01-20 22:08:40 -0500

    Server updates.

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@
 
 set IS_SERVER 'false'
 switch (hostname)
-    case 'box.nicksergeant.com'
+    case 'snipt.net'
         set IS_SERVER 'true'
 end
 
@@ -227,7 +227,6 @@ alias ec2 'ssh nick@ec2.nicksergeant.com'
 alias gf 'ssh nick@garthfagandance.org'
 alias pro 'ssh Nick@pro.local'
 alias sn 'ssh nick@snipt.net'
-alias box 'ssh nick@box.nicksergeant.com'
 
 # }}}
 # Z {{{
