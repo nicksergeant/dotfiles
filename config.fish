c2f1993ff3bf57880f42e3ad20caf50f3bf68d1d
Nick Sergeant <nick@nicksergeant.com>
2013-10-29 00:35:24 -0400

    Rubies?

--- config.fish
+++ config.fish
@@ -334,67 +334,6 @@ set -g -x WORKON_HOME "$HOME/.virtualenvs"
 . ~/.config/fish/virtualenv.fish
 
 # }}}
-# Ruby {{{
-
-if test $IS_SERVER = 'false'
-    set -x PATH $HOME/.rbenv/bin $PATH
-    set -x PATH $HOME/.rbenv/shims $PATH
-    rbenv rehash >/dev/null ^&1
-end
-
-function rbenv_shell
-  set -l vers $argv[1]
-
-  switch "$vers"
-    case '--complete'
-      echo '--unset'
-      echo 'system'
-      command rbenv versions --bare
-      return
-    case '--unset'
-      set -e RBENV_VERSION
-      return 1
-    case ''
-      if [ -z "$RBENV_VERSION" ]
-        echo "rbenv: no shell-specific version configured" >&2
-        return 1
-      else
-        echo "$RBENV_VERSION"
-        return
-      end
-    case '*'
-      rbenv prefix "$vers" > /dev/null
-      set -gx RBENV_VERSION "$vers"
-  end
-end
-
-function rbenv_lookup
-  set -l vers (command rbenv versions -- bare| sort | grep -- "$argv[1]" | tail -n1)
-
-  if [ ! -z "$vers" ]
-    echo $vers
-    return
-  else
-    echo $argv
-    return
-  end
-end
-
-function rbenv
-  set -l command $argv[1]
-  [ (count $argv) -gt 1 ]; and set -l args $argv[2..-1]
-
-  switch "$command"
-    case shell
-      rbenv_shell (rbenv_lookup $args)
-    case local global
-      command rbenv $command (rbenv_lookup $args)
-    case '*'
-      command rbenv $command $args
-  end
-end
-
-# }}}
 # Server functions {{{
 
 function afa 
