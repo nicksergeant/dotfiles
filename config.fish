034140ffa0780e66bf49742c3ac2a816308a64fe
Nick Sergeant <nick@nicksergeant.com>
2014-10-31 10:10:28 -0400

    Cleanup config.

--- config.fish
+++ config.fish
@@ -1,285 +1,169 @@
 # Bind Keys {{{
 
 function fish_user_keybindings
-    bind \cn accept-autosuggestion
-
-    # Ignore iterm2 escape sequences.  Vim will handle them if needed.
-    bind \e\[I true
-    bind \e\[O true
-    # ]]
-end
-
-# }}}
-# Ctags {{{
-
-function atags
-    unlink media/js/closure-library;
-    ctags -R .;
-    jsctags media/js -f jstags --ignore 'jquery|.*closure.*|.*.un~|.*.min.js|amara.js|popcorn.js|mirosubs-statwidget.js|unisubs-calcdeps.js|unisubs-statwidget.js';
-    cat jstags >> tags;
-    rm jstags;
-    ln -s /opt/google-closure media/js/closure-library;
-end
-
-function stags
-    ctags -R .;
-    jsctags media/js -f jstags --ignore 'jquery|codemirror|media/js/snipt.js|.*.un~|.*.min.js';
-    cat jstags >> tags;
-    rm jstags;
+  bind \cn accept-autosuggestion
+  bind \e\[I true
+  bind \e\[O true
+  # ]]
 end
 
 # }}}
 # Directories {{{
 
 function l
-    tree --dirsfirst -ChFL 1 $args
+  tree --dirsfirst -ChFL 1 $args
 end
 function ll
-    tree --dirsfirst -ChFupDaL 1 $args
-end
-
-function logs
-    cd /Users/Nick/Sources/dotfiles-private/weechat/logs
-end
-
-function jslines
-    find . -name '*.js' | xargs wc -l
-end
-
-# }}}
-# Edit file functions {{{
-
-function ef
-    mvim ~/.config/fish/config.fish
-end
-function ev
-    mvim ~/.vimrc
+  tree --dirsfirst -ChFupDaL 1 $args
 end
 
 # }}}
 # Environment variables {{{
 
 set TMPDIR "/tmp"
-set PATH "/usr/local/bin"          $PATH
-set PATH "/usr/local/sbin"         $PATH
+set PATH "/usr/local/bin" $PATH
+set PATH "/usr/local/sbin" $PATH
 
 set BROWSER open
 set PATH "/usr/local/opt/ruby/bin" $PATH
-set PATH "/Applications/Postgres.app/Contents/MacOS/bin" $PATH
 set PATH "/Users/Nick/Sources/dotfiles/bin" $PATH
 set PATH "/usr/local/share/npm/bin" $PATH
-set PATH "/Users/Nick/Android/sdk/tools" $PATH
-set PATH "/Users/Nick/Android/sdk/platform-tools" $PATH
 
 set -g -x fish_greeting ''
 set -g -x EDITOR vim
 set -g -x NODE_PATH "/usr/local/lib/node_modules"
-set -g -x NODE_PATH "/usr/local/lib/jsctags/" $NODE_PATH
 
 # }}}
 # Git and Mercurial functions {{{
 
 function git
-    hub $argv
-end
-function gpds
-    git push; gulp deploy --env=staging
-end
-function g 
-    git $argv
-end
-function gm
-    git co master $argv
+  hub $argv
 end
 function gca 
-    git commit -a $argv
+  git commit -a $argv
 end
 function gc 
-    git compare (git rev-parse --abbrev-ref HEAD)
+  git compare (git rev-parse --abbrev-ref HEAD)
 end
 function gco 
-    git checkout $argv
+  git checkout $argv
 end
 function gd 
-    git diff HEAD
+  git diff HEAD
 end
 function gl
-    git pull $argv
-end
-function gll 
-    git submodule foreach git pull $argv
-end
-function glc 
-    /Users/Nick/Sources/dotfiles/bin/get_last_commit | pbcopy $argv
+  git pull $argv
 end
 function glco 
-    git browse -- commit/(/Users/Nick/Sources/dotfiles/bin/get_last_commit) $argv
+  git browse -- commit/(/Users/Nick/Sources/dotfiles/bin/get_last_commit) $argv
 end
 function glu
-    git checkout master;
-    git pull;
-    git checkout -;
+  git checkout master;
+  git pull;
+  git checkout -;
 end
 function gp 
-    git push $argv
+  git push $argv
 end
 function gst 
-    git status $argv
+  git status $argv
 end
+
+# }}}
+# GitHub Issues {{{
+
 function i
-    ghi $argv
+  ghi $argv
 end
 function ic
-    ghi comment -l $argv
+  ghi comment -l $argv
 end
 function ilm
-    ghi list --mine $argv
+  ghi list --mine $argv
 end
 function il
-    ghi list $argv
+  ghi list $argv
 end
 function ilo
-    ghi list -w $argv
+  ghi list -w $argv
 end
 function is
-    ghi show $argv
+  ghi show $argv
 end
 function io
-    ghi show -w $argv
+  ghi show -w $argv
 end
 function ild
-    ghi list --milestone --label dev
+  ghi list --milestone --label dev
 end
 function ila
-    ghi list --all --mine $argv
+  ghi list --all --mine $argv
 end
 
 # }}}
 # Program functions {{{
 
 function awsm
-    cd ~/Code/awsm; bundle install; bundle exec rake awsm:mocked;
-end
-function c 
-    pygmentize -O style=monokai -f console256 -g $argv
-end
-function ce 
-    /Users/Nick/Code/unisubs/media/js/embedder/compile-embedder.sh $argv
-end
-function deact 
-    deactivate $argv
-end
-function gpda
-    git push; make deploy-with-assets;
+  cd ~/Code/awsm; bundle install; bundle exec rake awsm:mocked;
 end
 function gpd
-    git push; make deploy;
+  git push; make deploy;
 end
 function doge
-    suchvalue DAqKq1SG9abegwcpPEcdmYsr4NWfZSZLA6=dogehouse DAYrpmB2mVGZeRdLRmz2Jwf5VccN7t3nRf=cryptsy DT45nQ43qBCGbPS9ud4JXBKAMUMsnq6MuU=suchvalue DEm9MsUZ3U6mLhX1oi4QKmW6wNbB7fxeZH=dogetipbot DSwDw22PgAHD6wLzh6x2aSBuZSagM2EMKn=tipdoge DFebfjwBLp248Rr4fZ3yXJHg4B25N9Npau=cryptsy_fork
-end
-function h 
-    heroku $argv
-end
-function ip 
-    http icanhazip.com $argv
+  suchvalue DAqKq1SG9abegwcpPEcdmYsr4NWfZSZLA6=dogehouse DAYrpmB2mVGZeRdLRmz2Jwf5VccN7t3nRf=cryptsy DT45nQ43qBCGbPS9ud4JXBKAMUMsnq6MuU=suchvalue DEm9MsUZ3U6mLhX1oi4QKmW6wNbB7fxeZH=dogetipbot DSwDw22PgAHD6wLzh6x2aSBuZSagM2EMKn=tipdoge DFebfjwBLp248Rr4fZ3yXJHg4B25N9Npau=cryptsy_fork
 end
 function m
-    mvim . $argv
-end
-function mc 
-    telnet localhost 11211 $argv
-end
-function mk 
-    mkdir -p $argv
+  mvim . $argv
 end
 function mutt
-    bash --login -c 'cd ~/Downloads; /usr/local/bin/mutt' $argv;
+  bash --login -c 'cd ~/Downloads; /usr/local/bin/mutt' $argv;
 end
-function network_usage 
-    lsof -i | grep -E "(LISTEN|ESTABLISHED)" $argv
-end
-function o 
-    open $argv
-end
-function oo 
-    open . $argv
+function o
+  open $argv
 end
 function pbc 
-    pbcopy $argv
+  pbcopy $argv
 end
-function pbp 
-    pbpaste $argv
+function pbp
+  pbpaste $argv
 end
-function p
-    dtach -A /tmp/pianobar pianobar
-end
-function pm 
-    python manage.py $argv
+function pm
+  python manage.py $argv
 end
 function lisp
-    rlwrap sbcl
+  rlwrap sbcl
 end
 function ssc
-    sudo supervisorctl -c /Users/Nick/Sources/dotfiles-private/supervisor/supervisord.conf $argv
-end
-function sg
-    sgcli $argv
-end
-function sleep
-    sudo dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend
-end
-function stone
-    cd /Users/Nick/Code/stonevault/vault;
-    workon stonevault;
-    set -x DJANGO_SETTINGS_MODULE 'config.dev.settings';
-    set -x PYTHONPATH ':/usr/local/lib/python2.7/site-packages:/Users/Nick/Code/stonevault:/Users/Nick/Code/stonevault/vault:/Users/Nick/Code/stonevault/vault/apps:/Users/Nick/Code/stonevault/vault/apps/config';
-    django-admin.py runserver;
+  sudo supervisorctl -c /Users/Nick/Sources/dotfiles-private/supervisor/supervisord.conf $argv
 end
 function swift
-    xcrun swift
-end
-function syncdrives 
-    sudo rsync -avP /Volumes/Story/ /Volumes/Seagate $argv
+  xcrun swift
 end
 function ta 
-    tmux attach -t $argv
+  tmux attach -t $argv
 end
-function tk 
-    tmux kill-session -t $argv
-end
-function tn 
-    tmux new -s $argv
-end
-function ul 
-    unlink $argv
+function ti
+  tmux new-session -d -s primary
+  tmux rename-window -t primary shell
+  tmux attach
 end
 function vim
-    mvim -v $argv
+  mvim -v $argv
 end
 function vu
-    vagrant up $argv
+  vagrant up $argv
 end
 function vh
-    vagrant halt $argv
+  vagrant halt $argv
 end
 function vs
-    vagrant up; vagrant ssh $argv
-end
-function vst
-    vagrant status $argv
+  vagrant up; vagrant ssh $argv
 end
 function virtualbox_shut_down_or_i_will_fucking_cut_you
-    VBoxManage controlvm $argv poweroff $argv
-end
-function virtualbox_shut_down_or_i_will_fucking_cut_you
-    VBoxManage controlvm $argv poweroff
+  VBoxManage controlvm $argv poweroff $argv
 end
 function wo 
-    workon (cat .venv) $argv
-end
-function wakepc
-    wakeonlan 68:b5:99:d3:ab:35
+  workon (cat .venv) $argv
 end
 
 # }}}
@@ -289,98 +173,35 @@ set -x fish_color_command 005fd7\x1epurple
 set -x fish_color_search_match --background=purple
 
 function virtualenv_prompt
-    if [ -n "$VIRTUAL_ENV" ]
-        printf '\033[0;37m(%s) ' (basename "$VIRTUAL_ENV") $argv
-    end
+  if [ -n "$VIRTUAL_ENV" ]
+    printf '\033[0;37m(%s) ' (basename "$VIRTUAL_ENV") $argv
+  end
 end
 
 function git_prompt
-    if test $PWD = '/Users/Nick/Code/awsm'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/broker'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/cds'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/collabmatch'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/kony'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/humanitybox'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/mezo'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/nextgen-ui'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/stonevault'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
-    if test $PWD = '/Users/Nick/Code/publet'
-        set -l CUR (git currentbranch ^/dev/null)
-        printf ' \033[0;37mon '
-        printf '\033[0;35m%s' $CUR
-        printf ' \033[0;32m'
-        git_prompt_status
-    end
+  set -l CUR (git currentbranch ^/dev/null)
+  if test $CUR
+    printf ' \033[0;37mon '
+    printf '\033[0;35m%s' $CUR
+    printf ' \033[0;32m'
+    git_prompt_status
+  end
 end
 
 function prompt_pwd --description 'Print the current working directory, shortend to fit the prompt'
-    echo $PWD | sed -e "s|^$HOME|~|"
+  echo $PWD | sed -e "s|^$HOME|~|"
 end
 
 function fish_prompt
-    z --add "$PWD"
-    echo ' '
-    printf '\033[0;33m%s\033[0;37m on ' (whoami)
-    printf '\033[0;33m%spro.local '
-    printf '\033[0;32m%s' (prompt_pwd)
-    git_prompt
-    echo
-    virtualenv_prompt
-    printf '\033[0;37m> '
+  z --add "$PWD"
+  echo ' '
+  printf '\033[0;33m%s\033[0;37m on ' (whoami)
+  printf '\033[0;33m%spro.local '
+  printf '\033[0;32m%s' (prompt_pwd)
+  git_prompt
+  echo
+  virtualenv_prompt
+  printf '\033[0;37m> '
 end
 
 # }}}
@@ -404,25 +225,25 @@ function rbenv_shell
   set -l vers $argv[1]
 
   switch "$vers"
-    case '--complete'
-      echo '--unset'
-      echo 'system'
-      command rbenv versions --bare
-      return
-    case '--unset'
-      set -e RBENV_VERSION
+  case '--complete'
+    echo '--unset'
+    echo 'system'
+    command rbenv versions --bare
+    return
+  case '--unset'
+    set -e RBENV_VERSION
+    return 1
+  case ''
+    if [ -z "$RBENV_VERSION" ]
+      echo "rbenv: no shell-specific version configured" >&2
       return 1
-    case ''
-      if [ -z "$RBENV_VERSION" ]
-        echo "rbenv: no shell-specific version configured" >&2
-        return 1
-      else
-        echo "$RBENV_VERSION"
-        return
-      end
-    case '*'
-      rbenv prefix "$vers" > /dev/null
-      set -gx RBENV_VERSION "$vers"
+    else
+      echo "$RBENV_VERSION"
+      return
+    end
+  case '*'
+    rbenv prefix "$vers" > /dev/null
+    set -gx RBENV_VERSION "$vers"
   end
 end
 
@@ -453,40 +274,41 @@ function rbenv
 end
 
 # }}}
-# Server functions {{{
+# VMs and servers {{{
 
 function s
-  if test $argv = 'snipt'
-    ssh nick@snipt.net
-  else if test $argv = 'broker'
-    ssh nick@broker.is
-  else if test $argv = 'cds'
-    ssh nick@new.compliantdatasystems.com
-  else if test $argv = 'humanitybox'
-    ssh nick@humanitybox.com
+  switch "$argv"
+    case 'snipt'
+      ssh nick@snipt.net
+    case 'broker'
+      ssh nick@broker.is
+    case 'cds'
+      ssh nick@new.compliantdatasystems.com
+    case 'humanitybox'
+      ssh nick@humanitybox.com
+    case 'ng-job'
+      ssh nick@ng-job.com
   end
 end
 function vm
-    cd ~/Code/$argv; make run;
+  cd ~/Code/$argv; make run;
 end
 
 function collabmatch
-    sudo killall node -15; cd ~/Code/collabmatch; gulp;
+  sudo killall node -15; cd ~/Code/collabmatch; gulp;
 end
 function fitzlimo
-    sudo killall node -15; cd ~/Code/fitzlimo; nodemon -x node server &; cd client; gulp watch;
+  sudo killall node -15; cd ~/Code/fitzlimo; nodemon -x node server &; cd client; gulp watch;
+end
+function stone
+  cd /Users/Nick/Code/stonevault/vault;
+  workon stonevault;
+  set -x DJANGO_SETTINGS_MODULE 'config.dev.settings';
+  set -x PYTHONPATH ':/usr/local/lib/python2.7/site-packages:/Users/Nick/Code/stonevault:/Users/Nick/Code/stonevault/vault:/Users/Nick/Code/stonevault/vault/apps:/Users/Nick/Code/stonevault/vault/apps/config';
+  django-admin.py runserver;
 end
 function ui
-    sudo killall node -9; cd ~/Code/nextgen-ui/api; nodemon server &; cd ../server; sudo nodemon server;
-end
-
-# }}}
-# Tmux {{{
-
-function ti
-    tmux new-session -d -s primary
-    tmux rename-window -t primary shell
-    tmux attach
+  sudo killall node -9; cd ~/Code/nextgen-ui/api; nodemon server &; cd ../server; sudo nodemon server;
 end
 
 # }}}
@@ -495,7 +317,7 @@ end
 source ~/Sources/z-fish/z.fish
 
 function j
-    z $argv
+  z $argv
 end
 
 # }}}
