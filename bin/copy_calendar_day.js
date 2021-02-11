eee0320cf4c3aec1f167c2c98c3fe2246e90f7f0
Nick Sergeant <nsergeant@hubspot.com>
2021-02-11 13:08:36 -0500

    Simple copy script for calendar.

---
+++ bin/copy_calendar_day.js
@@ -0,0 +1,44 @@
+const { execSync } = require('child_process');
+
+const dayText = execSync('pbpaste').toString();
+const day = dayText.split('\n')[0];
+
+const dayString = day.match(/\w+, \w+ \d+, \d{4}$/);
+
+if (dayString) {
+  const events = dayText.replace(dayString, '').split('\n---\n');
+
+  for (i in events) {
+    const event = events[i].trim();
+    const eventLines = event.split('\n');
+    const eventTime = eventLines[0]
+      .split(' - ')[0]
+      .replace(' AM', 'a')
+      .replace(' PM', 'p');
+    const eventTitle = eventLines[1].replace('❇️ ', '');
+
+    if (
+      !eventTime.includes('all-day') &&
+      !eventTitle.includes("Get Arlene's mail") &&
+      !eventTitle.includes('Focus Time') &&
+      !eventTitle.includes('Take garbage') &&
+      !eventTitle.includes('Water herbs')
+    ) {
+      let title = eventTitle;
+
+      if (eventTitle.includes('Lunch')) {
+        title = 'Workout';
+      }
+
+      if (event.includes('hubspot.zoom.us')) {
+        const zoomLink = event.match(/(https:\/\/hubspot.zoom.us)[^\s]+/);
+
+        if (zoomLink) {
+          title = `[${eventTitle}](${zoomLink[0]})`;
+        }
+      }
+
+      console.log(eventTime, title);
+    }
+  }
+}
