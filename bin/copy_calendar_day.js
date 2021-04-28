a88e75a47add2b11d3e38d50884c9b9d322ec8e4
Nick Sergeant <nsergeant@hubspot.com>
2021-04-28 08:55:17 -0400

    Go back to simple calendar day copy.

--- bin/copy_calendar_day.js
+++ bin/copy_calendar_day.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
-const open = require('open');
 
 const dayText = execSync('pbpaste').toString();
 const day = dayText.split('\n')[0];
@@ -7,14 +6,12 @@ const day = dayText.split('\n')[0];
 const dayString = day.match(/\w+, \w+ \d+, \d{4}$/);
 
 if (dayString) {
-  const eventsRaw = dayText.replace(dayString, '').split('\n---\n');
+  const events = dayText.replace(dayString, '').split('\n---\n');
 
   let firstEvent = true;
 
-  const events = [];
-
-  for (i in eventsRaw) {
-    const event = eventsRaw[i].trim();
+  for (i in events) {
+    const event = events[i].trim();
     const eventLines = event.split('\n');
     const eventTime = eventLines[0]
       .split(' - ')[0]
@@ -27,30 +24,25 @@ if (dayString) {
       !eventTitle.includes("Get Arlene's mail") &&
       !eventTitle.includes('Focus Time') &&
       !eventTitle.includes('Take garbage') &&
-      !eventTitle.includes('Water herbs') &&
-      !eventTitle.includes('Lunch (via Clockwise)')
+      !eventTitle.includes('Water herbs')
     ) {
-      let title = eventTitle.replace('&', 'and');
+      let title = eventTitle;
 
-      // if (event.includes('hubspot.zoom.us')) {
-      //   const zoomLink = event.match(/(https:\/\/hubspot.zoom.us)[^\s]+/);
+      if (eventTitle.includes('Lunch')) {
+        title = 'Workout';
+      }
 
-      //   if (zoomLink) {
-      //     title = `${eventTitle} ${zoomLink[0]}`;
-      //   }
-      // }
+      if (event.includes('hubspot.zoom.us')) {
+        const zoomLink = event.match(/(https:\/\/hubspot.zoom.us)[^\s]+/);
 
-      events.push(`${eventTime} ${title}`);
+        if (zoomLink) {
+          title = `${eventTitle} ${zoomLink[0]}`;
+        }
+      }
+
+      console.log(`${firstEvent ? '' : '- [ ] '}${eventTime} ${title}`);
 
       firstEvent = false;
     }
   }
-
-  let thingsUrl = '';
-
-  if (events.length) {
-    thingsUrl = `things:///add?titles=${events.join('\n')}&when=Today&tags=Meeting,Work`;
-  }
-
-  open(thingsUrl);
 }
