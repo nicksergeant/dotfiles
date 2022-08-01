73ae27192be5500234b490d8380c5118ad19dabd
Nick Sergeant <nsergeant@hubspot.com>
2022-08-01 10:57:00 -0400

    Stuff

--- scripts/copy-fantastical-calendar-day.js
+++ scripts/copy-fantastical-calendar-day.js
@@ -1,41 +1,28 @@
 import { execSync } from 'child_process';
 
-const MODE = 'things'; // 'things' or 'markdown'
-
 const dayText = execSync('pbpaste').toString();
 const day = dayText.split('\n')[0];
 const dayString = day.match(/\w+, \w+ \d+, \d{4}$/);
+const ignores = ['Lunch (via Clockwise)', 'Take garbage and recycling out'];
 
 if (dayString) {
   const eventsRaw = dayText.replace(dayString, '').split('\n---\n');
   const events = [];
 
-  let firstEvent = true;
-
   for (var i in eventsRaw) {
     const event = eventsRaw[i].trim();
     const eventLines = event.split('\n');
-    const eventTime = eventLines[0]
-      .split(' - ')[0]
-      .replace(' AM', 'a')
-      .replace(' PM', 'p');
+    const eventTime = eventLines[0].split(' - ')[0];
     const eventTitle = eventLines[1].replace('❇️ ', '');
-    const lastEvent = parseInt(i) === eventsRaw.length - 1;
 
-    if (!eventTime.includes('all-day')) {
+    if (
+      !eventTime.includes('all-day') &&
+      !ignores.find((ignore) => eventTitle === ignore)
+    ) {
       let title = eventTitle;
-
-      if (MODE === 'markdown') {
-        console.log(`[ ] ${eventTime} ${title}${!lastEvent ? '\n' : ''}`);
-      } else if (MODE === 'things') {
-        events.push(`${eventTime} ${title}`);
-      }
-
-      firstEvent = false;
+      events.push(`${eventTime} ${title}`);
     }
   }
 
-  if (MODE === 'things' && events.length) {
-    console.log(events.join('\n'));
-  }
+  console.log(events.join('\n'));
 }
