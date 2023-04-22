7522338d3ef0da92d8bc08f6b503b74b2392b3d0
Nick Sergeant <nick@nicksergeant.com>
2023-04-21 20:35:09 -0400

    Deno support and cleanup lua.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,150 +2,104 @@ local util = require("lspconfig/util")
 local Job = require("plenary.job")
 
 function getIsHubspotMachine()
-    local result = ""
-    local testing = {}
-    Job:new({
-        command = "ls",
-        args = {vim.env.HOME .. '/.isHubspotMachine'},
-        on_exit = function(j, return_val)
-          result = return_val
-          testing = j
-        end
-    }):sync()
-     
-    return return_val == 0
+  local result = ""
+  local testing = {}
+  Job:new({
+    command = "ls",
+    args = {vim.env.HOME .. '/.isHubspotMachine'},
+    on_exit = function(j, return_val)
+      result = return_val
+      testing = j
+    end
+  }):sync()
+
+  return return_val == 0
 end
 
 function getLogPath() return vim.lsp.get_log_path() end
 
 function getTsserverPath()
-    local result = "/lib/tsserver.js"
-    Job:new({
-        command = "bpx",
-        args = {"--path", "hs-typescript"},
-        on_exit = function(j, return_val)
-            local path = j:result()[1]
-            result = path .. result
-        end
-    }):sync()
+  local result = "/lib/tsserver.js"
+  Job:new({
+    command = "bpx",
+    args = {"--path", "hs-typescript"},
+    on_exit = function(j, return_val)
+      local path = j:result()[1]
+      result = path .. result
+    end
+  }):sync()
 
-    return result
+  return result
 end
 
-local on_attach = function(client, bufnr)
-    -- local function buf_set_keymap(...)
-    --     vim.api.nvim_buf_set_keymap(bufnr, ...)
-    -- end
-    -- local function buf_set_option(...)
-    --     vim.api.nvim_buf_set_option(bufnr, ...)
-    -- end
-    --
-    -- buf_set_keymap("n", "<space>gd", "<cmd>lua vim.lsp.buf.definition()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "<space>gi",
-    --                "<cmd>lua vim.lsp.buf.implementation()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "<space>gr", "<cmd>lua vim.lsp.buf.references()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "<space>ga", "<cmd>lua vim.lsp.buf.code_action()<CR>",
-    --                {noremap = true, silent = true})
-    -- buf_set_keymap("n", "<space>gsd",
-    --                "<cmd>lua vim.lsp.buf.show_line_diagnostics({ focusable = false })<CR>",
-    --                {noremap = true, silent = true})
-end
-
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local customPublishDiagnosticFunction = function(_, result, ctx, config)
-    local filter = function(fun, t)
-        local res = {}
-        for _, item in ipairs(t) do
-            if fun(item) then res[#res + 1] = item end
-        end
-
-        return res
+  local filter = function(fun, t)
+    local res = {}
+    for _, item in ipairs(t) do
+      if fun(item) then res[#res + 1] = item end
     end
-    local raw_diagnostics = result.diagnostics
 
-    local filtered_diagnostics = filter(function(diagnostic)
-        local diagnostic_code = diagnostic.code
-        local diagnostic_source = diagnostic.source
-        return not (diagnostic_code == 7016 and diagnostic_source ==
-                   "typescript")
-    end, raw_diagnostics)
+    return res
+  end
+  local raw_diagnostics = result.diagnostics
 
-    result.diagnostics = filtered_diagnostics
+  local filtered_diagnostics = filter(function(diagnostic)
+    local diagnostic_code = diagnostic.code
+    local diagnostic_source = diagnostic.source
+    return not (diagnostic_code == 7016 and diagnostic_source ==
+    "typescript")
+  end, raw_diagnostics)
 
-    return vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+  result.diagnostics = filtered_diagnostics
+
+  return vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
 end
 
 local isHubspotMachine = getIsHubspotMachine()
 
 if isHubspotMachine then
-
-require("lspconfig").tsserver.setup({
+  require("lspconfig").tsserver.setup({
     cmd = {
-        "typescript-language-server", "--log-level", -- A number indicating the log level (4 = log, 3 = info, 2 = warn, 1 = error). Defaults to `2`.
-        "2", "--tsserver-log-verbosity", "terse", -- Specify tsserver log verbosity (off, terse, normal, verbose). Defaults to `normal`. example: --tsserver-log-verbosity=verbose
-        "--tsserver-log-file", getLogPath(), "--tsserver-path",
-        getTsserverPath(), "--stdio"
+      "typescript-language-server", "--log-level",
+      "2", "--tsserver-log-verbosity", "terse",
+      "--tsserver-log-file", getLogPath(), "--tsserver-path",
+      getTsserverPath(), "--stdio"
     },
-    on_attach = on_attach,
-    root_dir = util.root_pattern(".git"),
+    root_dir = util.root_pattern("package.json"),
     filetypes = {
-        "javascript", "javascriptreact", "javascript.jsx", "typescript",
-        "typescriptreact", "typescript.tsx"
+      "javascript", "javascriptreact", "javascript.jsx", "typescript",
+      "typescriptreact", "typescript.tsx"
     },
     handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(
-            customPublishDiagnosticFunction, {
-                -- Disable virtual_text
-                -- virtual_text = false
-            })
+      ["textDocument/publishDiagnostics"] = vim.lsp.with(
+      customPublishDiagnosticFunction, {})
     },
     capabilities = capabilities
-
-})
+  })
 else
-require("lspconfig").tsserver.setup({})
+  require("lspconfig").tsserver.setup {
+    root_dir = util.root_pattern("package.json"),
+    single_file_support = false
+  }
 end
 
--- npm install -g graphql-language-service-cli
-require'lspconfig'.graphql.setup {}
-require'lspconfig'.tailwindcss.setup {}
+require("lspconfig").graphql.setup {}
+require("lspconfig").tailwindcss.setup {}
+require("lspconfig").yamlls.setup {}
+require("lspconfig").denols.setup {
+  root_dir = util.root_pattern("deno.json", "deno.jsonc")
+}
 
--- yarn global add yaml-language-server
-require'lspconfig'.yamlls.setup {}
 require("lspkind").init({})
 
-vim.lsp.handlers['textDocument/signatureHelp'] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'single'})
-
-vim.lsp.handlers['textDocument/hover'] =
-    vim.lsp.with(vim.lsp.handlers.hover, {border = 'single'})
-
-vim.api.nvim_set_keymap("n", "go",
-                        "<cmd>lua vim.lsp.buf.definition()<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "gr",
-                        "<cmd>lua vim.lsp.buf.references()<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "mv", "<cmd>lua vim.lsp.buf.rename()<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.hover()<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "ga",
-                        "<cmd>lua vim.lsp.buf.code_action()<CR>",
-                        {noremap = true, silent = true})
--- vim.api.nvim_set_keymap("n", "gi",
---                         "<cmd>lua vim.lsp.buf.implementation()<CR>",
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap("n", "<leader>gsd",
---                         "<cmd>lua vim.lsp.buf.show_line_diagnostics({ focusable = false })<CR>",
---                         {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "go", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "mv", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap = true, silent = true})
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'single'})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'single'})
--- nvim/lua/tools.lua
+++ nvim/lua/tools.lua
@@ -2,8 +2,8 @@ local api = vim.api
 
 local Job = require("plenary.job")
 local log = require("plenary.log").new({
-    plugin = "asset-bender",
-    use_console = false
+  plugin = "asset-bender",
+  use_console = false
 })
 
 local M = {}
@@ -11,110 +11,105 @@ local M = {}
 M.path_sep = vim.loop.os_uname().sysname == "Windows" and "\\" or "/"
 
 function M.LSPLogs()
-    local logFile = vim.lsp.get_log_path()
-    api.nvim_command("split|term tail -f " .. logFile)
+  local logFile = vim.lsp.get_log_path()
+  api.nvim_command("split|term tail -f " .. logFile)
 end
 
 function M.GitRoot()
-    local git_root = vim.fn.systemlist("git -C " .. vim.loop.cwd() ..
-                                           " rev-parse --show-toplevel")[1]
-    local project_directory = git_root
-    if not git_root then project_directory = vim.loop.cwd() end
-    return project_directory
+  local git_root = vim.fn.systemlist("git -C " .. vim.loop.cwd() ..
+  " rev-parse --show-toplevel")[1]
+  local project_directory = git_root
+  if not git_root then project_directory = vim.loop.cwd() end
+  return project_directory
 end
 
 function M.telescope_files()
-    local root = M.GitRoot()
-    require("telescope.builtin").find_files({cwd = root})
+  local root = M.GitRoot()
+  require("telescope.builtin").find_files({cwd = root})
 end
 
 function M.telescope_grep()
-    local root = M.GitRoot()
-    require("telescope.builtin").live_grep({cwd = root})
+  local root = M.GitRoot()
+  require("telescope.builtin").live_grep({cwd = root})
 end
 
 function M.telescope_buffers()
-    local root = M.GitRoot()
-    require('telescope.builtin').buffers({initial_mode = "normal"})
+  local root = M.GitRoot()
+  require('telescope.builtin').buffers({initial_mode = "normal"})
 end
 
 function M.telescope_diagnostics(opts)
-    opts = {}
-    local utils = require "telescope.utils"
-    local pickers = require "telescope.pickers"
-    local locations = utils.diagnostics_to_tbl(opts)
+  opts = {}
+  local utils = require "telescope.utils"
+  local pickers = require "telescope.pickers"
+  local locations = utils.diagnostics_to_tbl(opts)
 
-    if vim.tbl_isempty(locations) then
-        print "No diagnostics found"
-        return
-    end
+  if vim.tbl_isempty(locations) then
+    print "No diagnostics found"
+    return
+  end
 
-    opts.path_display = utils.get_default(opts.path_display, "hidden")
-    pickers.new(opts, {
-        prompt_title = "LSP Document Diagnostics",
-        finder = require('telescope.finders').new_table {
-            results = locations,
-            entry_maker = opts.entry_maker or
-                require("telescope.make_entry").gen_from_lsp_diagnostics(opts)
-        },
-        previewer = require('telescope.previewers').new_termopen_previewer({
-            get_command = function(entry, status)
-                return {'echo', entry.text}
-            end
-        })
-    }):find()
+  opts.path_display = utils.get_default(opts.path_display, "hidden")
+  pickers.new(opts, {
+    prompt_title = "LSP Document Diagnostics",
+    finder = require('telescope.finders').new_table {
+      results = locations,
+      entry_maker = opts.entry_maker or
+      require("telescope.make_entry").gen_from_lsp_diagnostics(opts)
+    },
+    previewer = require('telescope.previewers').new_termopen_previewer({
+      get_command = function(entry, status)
+        return {'echo', entry.text}
+      end
+    })
+  }):find()
 
 end
 
--- Asumes filepath is a file.
 local function dirname(filepath)
-    local is_changed = false
-    local result = filepath:gsub(M.path_sep .. "([^" .. M.path_sep .. "]+)$",
-                                 function()
-        is_changed = true
-        return ""
-    end)
-    return result, is_changed
+  local is_changed = false
+  local result = filepath:gsub(M.path_sep .. "([^" .. M.path_sep .. "]+)$",
+  function()
+    is_changed = true
+    return ""
+  end)
+  return result, is_changed
 end
 
--- Ascend the buffer's path until we find the rootdir.
--- is_root_path is a function which returns bool
 function M.buffer_find_root_dir(bufnr, is_root_path)
-    local bufname = vim.api.nvim_buf_get_name(bufnr)
-    if vim.fn.filereadable(bufname) == 0 then return nil end
-    local dir = bufname
-    -- Just in case our algo is buggy, don't infinite loop.
-    for _ = 1, 100 do
-        local did_change
-        dir, did_change = dirname(dir)
-        if is_root_path(dir, bufname) then return dir, bufname end
-        -- If we can't ascend further, then stop looking.
-        if not did_change then return nil end
-    end
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if vim.fn.filereadable(bufname) == 0 then return nil end
+  local dir = bufname
+  for _ = 1, 100 do
+    local did_change
+    dir, did_change = dirname(dir)
+    if is_root_path(dir, bufname) then return dir, bufname end
+    if not did_change then return nil end
+  end
 end
 
 function M.path_join(...) return
-    table.concat(vim.tbl_flatten({...}), M.path_sep) end
+  table.concat(vim.tbl_flatten({...}), M.path_sep) end
 
-function M.file_exists(fname)
+  function M.file_exists(fname)
     local stat = vim.loop.fs_stat(fname)
     return (stat and stat.type) or false
-end
+  end
 
-function M.is_dir(filename)
+  function M.is_dir(filename)
     local stat = vim.loop.fs_stat(filename)
     return stat and stat.type == "directory" or false
-end
+  end
 
-function M.open_file(file) vim.cmd("e " .. file) end
+  function M.open_file(file) vim.cmd("e " .. file) end
 
-function M.FileExplorer()
+  function M.FileExplorer()
     if M.FileExplorerHasAlreadyBeenOpened == true then
-        vim.cmd("Rex")
+      vim.cmd("Rex")
     else
-        vim.cmd("Ex")
-        M.FileExplorerHasAlreadyBeenOpened = true;
+      vim.cmd("Ex")
+      M.FileExplorerHasAlreadyBeenOpened = true;
     end
-end
+  end
 
-return M
+  return M
