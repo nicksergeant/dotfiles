b91638d0a1144e5b3f2f8bedd653e90a84ade50e
Nick Sergeant <nick@nicksergeant.com>
2023-09-25 19:34:58 -0400

    Add script for Things and Linear.

---
+++ scripts/add-things-item-from-linear.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S /opt/homebrew/bin/deno run --allow-run
+
+import { encodeUrl } from 'https://deno.land/x/encodeurl/mod.ts';
+import { exec } from 'https://deno.land/x/exec/mod.ts';
+import { readLines } from 'https://deno.land/std@0.202.0/io/mod.ts';
+
+for await (const markdownLink of readLines(Deno.stdin)) {
+  let [title, url] = markdownLink.split('](');
+  title = encodeUrl(
+    title.replace('\\[', '[').replace('\\]', ']').split(' : ')[1]
+  );
+  url = url.replace(url[url.length - 1], '').replace('https://', 'linear://');
+  await exec(
+    `open things:///add?title=${title}&notes=${url}&when=today&show-quick-entry=true&list=FLX%20Websites`
+  );
+}
