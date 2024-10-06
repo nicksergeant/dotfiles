69def9f240a5920add1818821f35d34df3b5979c
Nick Sergeant <nick@nicksergeant.com>
2024-10-06 10:02:23 -0400

    Updates

--- scripts/add-things-item-from-linear.ts
+++ scripts/add-things-item-from-linear.ts
@@ -6,7 +6,11 @@ import { readLines } from 'https://deno.land/std@0.202.0/io/mod.ts';
 
 for await (const markdownLink of readLines(Deno.stdin)) {
   let [title, url] = markdownLink.split('](');
-  title = title.replace('\\[', '[').replace('\\]', ']').split(' : ')[1];
+  title = title
+    .replace('\\[', '[')
+    .replace('\\]', ']')
+    .split(': ')[1]
+    .replaceAll('"', "'");
   url = url.replace(url[url.length - 1], '').replace('https://', 'linear://');
   await exec(
     `open "things:///add?title=${title}&notes=${url}&when=today&show-quick-entry=true&list=FLX Websites"`
